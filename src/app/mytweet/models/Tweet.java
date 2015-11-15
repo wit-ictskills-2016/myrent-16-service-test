86eaa8b3237b10bad7e847f7f63e76ab50959cf0
John Fitzgerald <usplitu@hotmail>
2015-11-15 12:57:50 +0000

    alligned model instance var names with service app

--- src/app/mytweet/models/Tweet.java
+++ src/app/mytweet/models/Tweet.java
@@ -1,4 +1,5 @@
 package app.mytweet.models;
+
 import java.util.UUID;
 
 import com.google.common.base.Objects;
@@ -8,37 +9,39 @@ import static com.google.common.base.Objects.toStringHelper;
 public class Tweet
 {
 
-  public String   id;
-  public String   tweetText;
-  public String   tweetCount;
-  public String   tweetDate;
-  
-  public User   user;
-  
-  public Tweet(){}
-  
-  public Tweet(String tweetText, String tweetCount, String tweetDate)
+  public String id;
+  public String message;
+  public String count;
+  public String datestamp;
+
+  public User user;
+
+  public Tweet()
   {
-    this.id         = UUID.randomUUID().toString();
-    this.tweetText  = tweetText;
-    this.tweetCount = tweetCount;
-    this.tweetDate  = tweetDate;
   }
-  
+
+  public Tweet(String message, String count, String datestamp)
+  {
+    this.id = UUID.randomUUID().toString();
+    this.message = message;
+    this.count = count;
+    this.datestamp = datestamp;
+  }
+
   @Override
   public boolean equals(final Object obj)
   {
     if (obj instanceof Tweet)
     {
-      final Tweet other = (Tweet) obj;        
-      return     Objects.equal(id,         other.id)
-              && Objects.equal(tweetText,  other.tweetText)
-              && Objects.equal(tweetCount, other.tweetCount)
-              && Objects.equal(tweetDate,  other.tweetDate) ;                  
+      final Tweet other = (Tweet) obj;
+      return Objects.equal(id, other.id) 
+          && Objects.equal(message, other.message) 
+          && Objects.equal(count, other.count)
+          && Objects.equal(datestamp, other.datestamp);
     }
     else
     {
       return false;
     }
-  }  
+  }
 }
